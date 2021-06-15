b857b7db96d376e3c3b987ad6b99f148dc7ad6bf
Sai Santhosh <saisanthosh2504@gmail.com>
2021-06-15 18:56:03 +0530

    step2

--- Part 1/unicafe/step2/src/App.js
+++ Part 1/unicafe/step2/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [diff, setDiff] = useState(0)
+  
+  const handleGoodCounter = () =>{
+    setGood(good + 1)
+    setAll(all + 1)
+    setDiff(diff + 1)
+  }
+
+  const handleNeutralCounter = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadCounter = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setDiff(diff - 1)  
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodCounter} text="good" />
+      <Button handleClick={handleNeutralCounter} text="neutral" />
+      <Button handleClick={handleBadCounter} text="bad" />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad} </p>
+      <p>all {all}</p>
+      <p>average {diff/all}</p>
+      <p>positive {good*100/all} %</p>
     </div>
-  );
+  )
 }
 
-export default App;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+
+export default App
