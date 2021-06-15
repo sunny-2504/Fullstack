549574cb873838fb002b150df29a437c2b314401
Sai Santhosh <saisanthosh2504@gmail.com>
2021-06-15 19:51:14 +0530

    step5

--- Part 1/unicafe/step5/src/App.js
+++ Part 1/unicafe/step5/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [diff, setDiff] = useState(0)
+  
+  const handleGoodCounter = () =>{
+    setGood(good + 1)
+    setAll(all + 1)
+    setDiff(diff + 1)
+  }
+
+  const handleNeutralCounter = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadCounter = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setDiff(diff - 1) 
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodCounter} text="good" />
+      <Button handleClick={handleNeutralCounter} text="neutral" />
+      <Button handleClick={handleBadCounter} text="bad" />
+      <h2>statistics</h2>
+      <Statistics good ={good} neutral = {neutral} bad = {bad} all = {all} diff = {diff} />
     </div>
-  );
+  )
 }
 
-export default App;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = (props) => {
+  if(props.all === 0){
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Statistic text="good" value={props.good} />
+      <Statistic text="neutral" value={props.neutral} />
+      <Statistic text="bad" value={props.bad} />
+      <Statistic text="all" value={props.all} />
+      <Statistic text="average" value={props.diff/props.all} />
+      <Statistic text="positive" value={props.good*100/props.all}  />
+    </div>
+  )
+
+}
+
+const Statistic = (props) => {
+  const text = props.text
+  if(text === 'positive') {
+    return (
+      <p>{text} {props.value} % </p>
+    )
+  }
+  return (
+    <p>{text} {props.value}</p>
+  )
+
+}
+
+
+
+export default App
