8e35c00209c125e4588ea4c2721cbdfa55aa41f0
sunny-2504 <saisanthosh2504@gmail.com>
2022-07-01 11:45:45 +0530

    part2

--- Part 1/anecdotes/step1/src/App.js
+++ Part 1/anecdotes/step1/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -8,7 +9,7 @@ const App = () => {
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients'
   ]
    
   const [selected, setSelected] = useState(0)
@@ -22,10 +23,20 @@ const App = () => {
   }
   console.log(selected)
 
+  const handleclick2 =() =>{
+    if(selected === 0){
+      setSelected(anecdotes.length-1)
+    }
+    else{
+      setSelected(selected - 1)
+    }
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
       <button onClick={handleclick}>next anecdote</button>
+      <button onClick={handleclick2}>previious anecdote</button>
     </div>
   )
 }
--- Part 1/anecdotes/step3/src/App.js
+++ Part 1/anecdotes/step3/src/App.js
@@ -22,6 +22,7 @@ const App = () => {
     else
       setSelected(selected + 1)
   }
+  
 
 
   const vote = () => {
